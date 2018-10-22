0d91809e4b31dec68a098cd0ed021f25e9422889
stevejamesson <steve@evendev.com>
2018-10-22 13:31:53 -0400

    misc

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+// Configure your 11ty input & output here
+const src = './src'
+const dist = './dist'
+
 module.exports = function (config) {
+  // 11ty I/O
   config.dir = {
-    input: './src',
-    output: './dist'
+    input: src,
+    output: dist,
   }
+
+  // passthrough files
+  // config.addPassthroughCopy('./src/static`)
 
   return config
 }
